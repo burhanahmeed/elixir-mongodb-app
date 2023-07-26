4805d20492bb372cfc75548c5fffb03fb69a1e92
Burhan Ahmed <burhanahmeed@gmail.com>
2023-07-26 20:00:27 +0700

    change prod config

--- config/prod.env.exs
+++ config/prod.env.exs
@@ -1,11 +1,11 @@
 import Config
 
 config :todo_app, port: 8081
-config :todo_app, database_url: "mongoservice:27017"
+config :todo_app, database_url: "host.docker.internal:27017"
 config :todo_app, database: "sultanads"
 config :todo_app, pool_size: 3
 config :todo_app, :basic_auth, username: "abc", password: "abc"
-config :todo_app, mysql_host: "mysqlservice"
+config :todo_app, mysql_host: "host.docker.internal"
 config :todo_app, mysql_username: "admin"
 config :todo_app, mysql_password: "d892j!k2iewA"
 config :todo_app, mysql_database: "demo_db"
