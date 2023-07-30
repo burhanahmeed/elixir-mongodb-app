dea595adb99d35e490a506c789551ffbfce588ca
Burhan Ahmed <burhanahmeed@gmail.com>
2023-07-30 09:51:11 +0700

    add to host docker

--- config/prod.env.exs
+++ config/prod.env.exs
@@ -1,11 +1,11 @@
 import Config
 
 config :todo_app, port: 8081
-config :todo_app, database_url: "localhost:27017"
+config :todo_app, database_url: "host.docker.internal:27017"
 config :todo_app, database: "sultanads"
 config :todo_app, pool_size: 3
 config :todo_app, :basic_auth, username: "abc", password: "abc"
-config :todo_app, mysql_host: "localhost"
+config :todo_app, mysql_host: "host.docker.internal"
 # config :todo_app, mysql_username: "root"
 config :todo_app, mysql_username: "admin"
 config :todo_app, mysql_password: "d892j!k2iewA"
