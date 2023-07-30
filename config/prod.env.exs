99329fcbe84fd1ae68038065e8bc94b321522633
Burhan Ahmed <burhanahmeed@gmail.com>
2023-07-30 21:40:44 +0700

    change production mongodb

--- config/prod.env.exs
+++ config/prod.env.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :todo_app, port: 8081
-config :todo_app, database_url: "host.docker.internal:27017"
+config :todo_app, database_url: "ac-ayvwbub-shard-00-02.qykuv69.mongodb.net:27017"
 config :todo_app, database: "sultanads"
 config :todo_app, pool_size: 3
 config :todo_app, :basic_auth, username: "abc", password: "abc"
@@ -12,3 +12,5 @@ config :todo_app, mysql_password: "d892j!k2iewA"
 config :todo_app, mysql_database: "demo_db"
 # config :todo_app, mysql_password: ""
 # config :todo_app, mysql_database: "dev_mdom"
+
+# db.createUser({user: "admin", pwd: "d892j!k2iewA", roles: [{ role: "readWrite", db: "sultanads" }]});
